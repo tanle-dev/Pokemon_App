9b94c125003a5925b1ce117bccd70b48180ce3fc
tanle-dev <lenhuttan.programing@gmail.com>
2024-07-23 12:50:50 -0400

    Set up load Pokemon list view model

--- app/src/main/java/ca/tanle/pokedex/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/ca/tanle/pokedex/pokemonlist/PokemonListViewModel.kt
@@ -3,17 +3,69 @@ package ca.tanle.pokedex.pokemonlist
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.capitalize
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.palette.graphics.Palette
+import ca.tanle.pokedex.data.models.PokedexListEntry
 import ca.tanle.pokedex.repository.PokemonRepository
+import ca.tanle.pokedex.utils.Constants.PAGE_SIZE
+import ca.tanle.pokedex.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class PokemonListViewModel @Inject constructor(
     private val repository: PokemonRepository
 ) : ViewModel() {
+
+    var currentPage = 0
+
+    var pokemonList = mutableStateOf<List<PokedexListEntry>>(listOf())
+    var loadError = mutableStateOf("")
+    var isLoading = mutableStateOf(false)
+    var endReached = mutableStateOf(false)
+
+    fun loadPokemonPaginated(){
+        viewModelScope.launch {
+            isLoading.value = true
+            val result = repository.getPokemonList(PAGE_SIZE, currentPage * PAGE_SIZE)
+            when(result){
+                is Resource.Success -> {
+                    endReached.value = currentPage * PAGE_SIZE >= result.data!!.count
+
+                    val pokeEntries = result.data.results.mapIndexed{ index, entry ->
+                        val number = if(entry.url.endsWith("/")){
+                            entry.url.dropLast(1).takeLastWhile { it.isDigit() }
+                        }else{
+                            entry.url.takeLastWhile { it.isDigit() }
+                        }
+
+                        val url = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png"
+
+                        PokedexListEntry(entry.name.capitalize(Locale.ROOT), url, number.toInt())
+                    }
+
+                    currentPage++
+                    loadError.value = ""
+                    isLoading.value = false
+                    pokemonList.value += pokeEntries
+                }
+                is Resource.Error -> {
+                    loadError.value = result.message!!
+                    isLoading.value = false
+                }
+
+                else -> {}
+            }
+        }
+    }
+
     fun calcDominantColor(drawable: Drawable, onFinish: (Color) -> Unit ){
 //        cast drawable to bitmap
         val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
--- app/src/main/java/ca/tanle/pokedex/utils/Constants.kt
+++ app/src/main/java/ca/tanle/pokedex/utils/Constants.kt
@@ -3,4 +3,5 @@ package ca.tanle.pokedex.utils
 object Constants {
 
     const val BASE_URL = "https://pokeapi.co/"
+    const val PAGE_SIZE = 20
 }
