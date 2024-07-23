9bc1f37d23caeb476b167e6502c83637552fbf86
tanle-dev <lenhuttan.programing@gmail.com>
2024-07-23 11:50:26 -0400

    Add Poke row

--- app/src/main/java/ca/tanle/pokedex/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/ca/tanle/pokedex/pokemonlist/PokemonListScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
@@ -189,6 +191,39 @@ fun PokedexEntry(
                 modifier = Modifier.fillMaxWidth()
             )
         }
+    }
+}
+
+
+@Composable
+fun PokedexRow(
+    rowIndex: Int,
+    entries: List<PokedexListEntry>,
+    navController: NavController
+){
+    Column {
+
+        Row {
+            PokedexEntry(
+                entry = entries[rowIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            
+            Spacer(modifier = Modifier.width(16.dp))
+
+            if(entries.size >= rowIndex * 2 + 2){
+                PokedexEntry(
+                    entry = entries[rowIndex * 2 + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            }else{
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+        
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
 
