2a30219db0f0ac3beeb44cf8991d1821ad9bf28c
tanle-dev <lenhuttan.programing@gmail.com>
2024-07-26 19:50:37 -0400

    set up detail screen

--- app/src/main/java/ca/tanle/pokedex/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/ca/tanle/pokedex/pokemondetail/PokemonDetailScreen.kt
@@ -1,4 +1,21 @@
 package ca.tanle.pokedex.pokemondetail
 
-class PokemonDetailScreen {
+import android.graphics.Color
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import ca.tanle.pokedex.data.remote.response.Pokemon
+
+@Composable
+fun PokemonDetailScreen(
+    dominantColor: Color,
+    pokemonName: String,
+    navController: NavController,
+    topPadding: Dp = 20.dp,
+    pokemonImageSize: Dp = 20.dp,
+    viewModel: PokemonDetailViewModel = hiltViewModel()
+    ) {
+
 }
